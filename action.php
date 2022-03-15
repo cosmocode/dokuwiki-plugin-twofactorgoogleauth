7482dc8e5b65512d7ded4bb1e506600d9129f9d8
Andreas Gohr <andi@splitbrain.org>
2022-03-15 09:50:25 +0100

    changed QRcode library

--- action.php
+++ action.php
@@ -3,9 +3,6 @@
 use dokuwiki\plugin\twofactor\Provider;
 use dokuwiki\Form\Form;
 
-// Load the PHP QR Code library.
-require_once(dirname(__FILE__).'/phpqrcode.php');
-
 /**
  * If we turn this into a helper class, it can have its own language and settings files.
  * Until then, we can only use per-user settings.
@@ -89,33 +86,9 @@ class action_plugin_twofactorgoogleauth extends Provider
         if (!$secret) {
             $ga = new dokuwiki\plugin\twofactor\GoogleAuthenticator();
             $secret = $ga->createSecret();
+            $this->settings->set('secret', $secret);
         }
         return $secret;
-    }
-
-    /**
-     * Generates the QR Code used by Google Authenticator and produces a data
-     * URI for direct insertion into the HTML source.
-     * @param $name - The email address fo the user
-     * @param $secret - The secret hash used to seed the otp formula
-     * @return string - a complete data URI to be placed in an img tag's src
-     *      attribute.
-     */
-    private function generateQRCodeData($name, $secret)
-    {
-        $url = 'otpauth://totp/'.rawurlencode($name).'?secret='.$secret;
-        // Capture PNG image for embedding into HTML.
-        // Thanks to https://evertpot.com/222/ for the stream tutorial
-        // First create a stream that the PNG will be written into.
-        $stream = fopen('php://memory', 'r+');
-        // Write the PNG into the stream
-        QRcode::png($url, $stream);
-        // Reset the stream to the start.
-        rewind($stream);
-        // Read in the contents of the stream.
-        $image_data = stream_get_contents($stream);
-        // Convert to data URI.
-        return "data:image/png;base64," . base64_encode($image_data);
     }
 
     /**
